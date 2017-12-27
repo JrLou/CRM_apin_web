2adef221664d9ac9e087fcfe9c2cf0d4494de7ea
yaunshaokai <1146997545@qq.com>
2017-12-27 18:20:05 +0800

    登陆界面

--- src/layouts/UserLayout.js
+++ src/layouts/UserLayout.js
@@ -4,7 +4,7 @@ import DocumentTitle from 'react-document-title';
 import { Icon } from 'antd';
 import GlobalFooter from '../components/GlobalFooter';
 import styles from './UserLayout.less';
-import logo from '../assets/logo.svg';
+import logo from '../assets/logo.png';
 import { getRoutes } from '../utils/utils';
 
 const links = [{
@@ -18,7 +18,7 @@ const links = [{
   href: '',
 }];
 
-const copyright = <div>Copyright <Icon type="copyright" /> 2017 蚂蚁金服体验技术部出品</div>;
+const copyright = <div>Copyright <Icon type="copyright" />2017 爱拼机小组出品</div>;
 
 class UserLayout extends React.PureComponent {
   getPageTitle() {
@@ -39,10 +39,10 @@ class UserLayout extends React.PureComponent {
             <div className={styles.header}>
               <Link to="/">
                 <img alt="logo" className={styles.logo} src={logo} />
-                <span className={styles.title}>Ant Design</span>
+                <span className={styles.title}>爱拼机</span>
               </Link>
             </div>
-            <div className={styles.desc}>Ant Design 是西湖区最具影响力的 Web 设计规范</div>
+            <div className={styles.desc}>爱拼机CRM登陆页面</div>
           </div>
           {
             getRoutes(match.path, routerData).map(item =>
