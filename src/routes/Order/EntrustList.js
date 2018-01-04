205806314907e0c1fd96c68e23c0f406ebd01f2a
leiniuniu <leiniuniu@apin.com>
2018-01-04 17:03:45 +0800

    qasa

--- src/routes/Order/EntrustList.js
+++ src/routes/Order/EntrustList.js
@@ -1,5 +1,6 @@
 import React, { PureComponent } from 'react';
 import moment from 'moment';
+import { Link } from 'dva/router';
 import { Table, Alert, Badge, Divider } from 'antd';
 import styles from './TableList.less';
 
@@ -44,7 +45,7 @@ class StandardTable extends PureComponent {
     const { selectedRowKeys, totalCallNo } = this.state;
     const { data: { list, pagination }, loading } = this.props;
 
-    const status = ['关闭', '运行中', '已上线', '异常'];
+    const status = ['运行中', '选择方案中', '待出票', '已出票', '委托取消', '出票失败', '委托过期'];
 
     const columns = [
       {
@@ -54,24 +55,6 @@ class StandardTable extends PureComponent {
       {
         title: '订单状态',
         dataIndex: 'status',
-        filters: [
-          {
-            text: status[0],
-            value: 0,
-          },
-          {
-            text: status[1],
-            value: 1,
-          },
-          {
-            text: status[2],
-            value: 2,
-          },
-          {
-            text: status[3],
-            value: 3,
-          },
-        ],
         render(val) {
           return <Badge status={statusMap[val]} text={status[val]} />;
         },
@@ -95,7 +78,6 @@ class StandardTable extends PureComponent {
       {
         title: '出发时间(下单)',
         dataIndex: 'startOff',
-        // sorter: true,
         render: val => <span>{moment(val).format('YYYY年MM月DD日')}</span>,
       },
       {
@@ -118,10 +100,10 @@ class StandardTable extends PureComponent {
       },
       {
         title: '操作',
-        render:(text, record, index) => {
+        render:(text, record, val) => {
           // 生成复杂数据的渲染函数，参数分别为当前行的值，当前行数据，行索引，@return里面可以设置表格行/列合并
-          if (!record.img_url) {
-              return <a href="">查看</a>
+          if (val != '3') {
+              return <Link to="/order/entrust/:id">查看</Link>
           } else {
               return <a href="">出票</a>
           }
