54c275744cb6c195c0acc77dad76a8367e000a00
wunayou <574612524@qq.com>
2018-01-04 17:21:04 +0800

    修改路由

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -111,18 +111,19 @@ export function getRoutes(path, routerData) {
   let routes = Object.keys(routerData).filter(routePath =>
     routePath.indexOf(path) === 0 && routePath !== path);
   routes = routes.map(item => item.replace(path, ''));
-  let renderArr = [];
-  renderArr.push(routes[0]);
-  for (let i = 1; i < routes.length; i += 1) {
-    let isAdd = false;
-    isAdd = renderArr.every(item => getRelation(item, routes[i]) === 3);
-    renderArr = renderArr.filter(item => getRelation(item, routes[i]) !== 1);
-    if (isAdd) {
-      renderArr.push(routes[i]);
-    }
-  }
-  const renderRoutes = renderArr.map((item) => {
-    const exact = !routes.some(route => route !== item && getRelation(route, item) === 1);
+  // let renderArr = [];
+  // renderArr.push(routes[0]);
+  // for (let i = 1; i < routes.length; i += 1) {
+  //   let isAdd = false;
+  //   isAdd = renderArr.every(item => getRelation(item, routes[i]) === 3);
+  //   renderArr = renderArr.filter(item => getRelation(item, routes[i]) !== 1);
+  //   if (isAdd) {
+  //     renderArr.push(routes[i]);
+  //   }
+  // }
+  const renderRoutes = routes.map((item) => {
+    // const exact = !routes.some(route => route !== item && getRelation(route, item) === 1);
+    const exact = true
     return {
       key: `${path}${item}`,
       path: `${path}${item}`,
