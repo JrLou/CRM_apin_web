32fa2bac306a8e4c9986de553f3fcd8a14cf9765
wunayou <574612524@qq.com>
2018-01-12 16:26:05 +0800

    sdsd

--- src/auth/AuthRoute.js
+++ src/auth/AuthRoute.js
@@ -63,7 +63,7 @@ export default (WrappedComponent, exacpath) => {
       }
     }
     render() {
-      return (this.state.load ? <WrappedComponent {...this.props} />:notauth?<NotAuth />:<Spin size="large" style={{ width: "100%",margin: "40px 0 !important"}} />)
+      return (this.state.load ? <WrappedComponent {...this.props} />:this.state.notauth?<NotAuth />:<Spin size="large" style={{ width: "100%",margin: "40px 0 !important"}} />)
     }
   }
   const A = connect(state => ({
