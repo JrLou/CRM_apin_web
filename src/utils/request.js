059cb0c85170ef7f85c390da80830ac67b6f0c1f
wunayou <574612524@qq.com>
2018-01-12 17:53:23 +0800

    blob

--- src/utils/request.js
+++ src/utils/request.js
@@ -119,7 +119,7 @@ export default function request(url, options) {
   return fetch(url, newOptions)
     .then(checkStatus)
     .then((response) => {
-      if(response.headers.get('Content-Type')==blob){
+      if(response.headers.get('Content-Type')=='blob'){
           return URL.createObjectURL(response.blob)
       }
       if (newOptions.method === 'DELETE' || response.status === 204) {
