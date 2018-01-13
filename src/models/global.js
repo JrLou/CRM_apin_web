4dd01661e250dcb312d36874d33228cc7b6180e9
wunayou <574612524@qq.com>
2018-01-13 16:24:39 +0800

    一键配置

--- src/models/global.js
+++ src/models/global.js
@@ -7,7 +7,7 @@ export default {
   namespace: 'global',
   state: {
     collapsed: false,
-    menusload: false,  //控制加载路由数据
+    menusload: false,  //控制导航数据
     env: false, // 控制进入每一个进入页面的权限
     menus: [],
     routerdata: [],
