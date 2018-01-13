7a51192b86028f35954037971e74690ee414e0b6
lisiqi6911660 <lisiqi6911660@163.com>
2018-01-13 13:50:20 +0800

    add

--- src/models/global.js
+++ src/models/global.js
@@ -1,7 +1,7 @@
-import { queryMenus,authrouteApi} from '../services/api';
+import {queryMenus, authrouteApi} from '../services/api';
 import CookieHelp from './../utils/cookies';
-import { routerRedux } from 'dva/router';
-import { debug } from 'util';
+import {routerRedux} from 'dva/router';
+import {debug} from 'util';
 
 export default {
   namespace: 'global',
@@ -11,37 +11,37 @@ export default {
     menusloaderr: false,
     menus: [],
     routerdata: [],
-    routerPath:[{path:'welcome',url:"welcome"},{url:'supplier/supplierPolicy/flightstock/Edit'}],
-    authlist:{
-      "customer/userList":{},
-      "supplier/supplierPolicy/flightstock":{},
-      "fightgroups/demand":{},
-      "fightgroups/list":{},
-      "order/flyingpig":{},
-      "order/entrust":{},
-      "order/refund":{},
-      "operations/banner":{}
+    routerPath: [{path: 'welcome', url: "welcome"}, {url: 'supplier/supplierPolicy/flightstock/Edit'}],
+    authlist: {
+      "customer/userList": {},
+      "supplier/supplierPolicy/flightstock": {},
+      "fightgroups/demand": {},
+      "fightgroups/list": {},
+      "order/flyingpig": {},
+      "order/entrust": {},
+      "order/refund": {},
+      "operations/banner": {}
     } // 用于权限控制的数据表
   },
 
   effects: {
-    *fetchMenus(_, { call, put }) {
+    * fetchMenus(_, {call, put}) {
       yield put({
         type: 'changeMenusLoading',
         payload: true,
       });
       const res = yield call(queryMenus);
       if (res && res.code >= 1) {
-        const { data } = res
+        const {data} = res
         yield put({
           type: 'changerouterPathData',
-          payload:data
+          payload: data
         })
-        data.forEach(item=>item.path=item.url)
+        data.forEach(item => item.path = item.url)
         let menus_children = data.filter(item => !item.parentId).map((element, index, arr) => {
           let children = data.filter(item => item.parentId == element.id)
-              element.children = children
-              return element
+          element.children = children
+          return element
         });
         yield put({
           type: 'changeMenusData',
@@ -51,44 +51,44 @@ export default {
           type: 'changeMenusload',
           payload: true
         })
-      }else{
+      } else {
         CookieHelp.clearCookie()
         yield put(routerRedux.push('/user/login'));
       }
     },
-    *authroute({ payload },{call,put,select}){
-      const  routerPath = yield select(state => state.global.routerPath);
-      const id = routerPath.filter(item=>item.parentId&&payload.indexOf(item.path)>-1)[0].id
-      const res = yield call(authrouteApi,{id});
+    * authroute({payload}, {call, put, select}) {
+      const routerPath = yield select(state => state.global.routerPath);
+      const id = routerPath.filter(item => item.parentId && payload.indexOf(item.path) > -1)[0].id
+      const res = yield call(authrouteApi, {id});
       if (res && res.code >= 1) {
-        const { data } = res
+        const {data} = res
         yield put({
           type: 'changerouterPathData',
-          payload:data
+          payload: data
         })
       }
     }
   },
   reducers: {
-    changeLayoutCollapsed(state, { payload }) {
+    changeLayoutCollapsed(state, {payload}) {
       return {
         ...state,
         collapsed: payload,
       };
     },
-    changeMenusload(state, { payload }) {
+    changeMenusload(state, {payload}) {
       return {
         ...state,
         menusload: payload,
       };
     },
-    changeMenusData(state, { payload }) {
+    changeMenusData(state, {payload}) {
       return {
         ...state,
         menus: payload,
       };
     },
-    changerouterPathData(state,{payload}){
+    changerouterPathData(state, {payload}) {
       return {
         ...state,
         routerPath: state.routerPath.concat(payload),
@@ -97,9 +97,9 @@ export default {
   },
 
   subscriptions: {
-    setup({ history }) {
+    setup({history}) {
       // Subscribe history(url) change, trigger `load` action if pathname is `/`
-      return history.listen(({ pathname, search }) => {
+      return history.listen(({pathname, search}) => {
         if (typeof window.ga !== 'undefined') {
           window.ga('send', 'pageview', pathname + search);
         }
