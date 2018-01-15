545b55cac557161e48bcbb07d0f467ec5e77c8e2
zhilingege <zhilingege@gmail.com>
2018-01-15 17:35:32 +0800

    微调

--- src/routes/CustomerMannagement/CustomerMannagement.js
+++ src/routes/CustomerMannagement/CustomerMannagement.js
@@ -2,10 +2,12 @@ import React, {PureComponent} from 'react';
 import {connect} from 'dva';
 import {
   Card,
+  Modal,
   Form,
   Input,
   Button,
-  message
+  Row,
+  Col
 } from 'antd';
 import StandardTable from './TableList';
 import PageHeaderLayout from '../../layouts/PageHeaderLayout';
@@ -23,6 +25,8 @@ export default class TableList extends PureComponent {
   state = {
     selectedRows: [],
     formValues: {},
+    modalFormValues: {},
+    visible: false,
   };
 
   componentDidMount() {
@@ -95,58 +99,177 @@ export default class TableList extends PureComponent {
     });
   };
 
+  handleModalSave = (e) => {
+    e.preventDefault();
+
+    const {dispatch, form} = this.props;
+
+    form.validateFields((err, fieldsValue) => {
+      if (err) return;
+      const values = {
+        ...fieldsValue,
+      };
+      this.setState({
+        modalFormValues: values,
+      });
+      dispatch({
+        type: 'customerMannagement/fetch',
+        payload: values,
+      });
+    });
+  };
+
   renderForm() {
     const {getFieldDecorator} = this.props.form;
-    const formItemStyle = {
-      width: "350px",
-      display: "inline-block"
-    };
-    const inputStyle = {
-      width: "220px",
-    };
+    const layoutForm = {md: 8, lg: 24, xl: 48};
+
     return (
       <Form onSubmit={this.handleSearch} layout="inline">
-        <FormItem label="客户名称:" style={formItemStyle}>
-          {getFieldDecorator('name', { //【客户名称】支持中文、英文、数字，最多50个字符；
+        <Row gutter={layoutForm}>
+          <Col md={8} sm={24}>
+            <FormItem label="客户名称:">
+              {getFieldDecorator('name', { //【客户名称】支持中文、英文、数字，最多50个字符；
+                initialValue: "",
+                rules: [{max: 50, message: '长度不能超过50'}],
+              })
+              (<Input placeholder="请输入"/>)
+              }
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="联系人:">
+              {getFieldDecorator('connect', {//【联系人】支持中文、英文，允许输入特殊字符，小写英文自动转换为大写，最多20个字符；
+                initialValue: "",
+                rules: [{max: 20, message: '长度不能超过20'}],
+              })
+              (<Input placeholder="请输入"/>)
+              }
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="手机号:">
+              {getFieldDecorator('mobile', {//【电话号码】支持数字，允许输入特殊字符，最多50个字符；
+                initialValue: "",
+                rules: [{
+                  pattern: /^\d{0,20}$/,
+                  message: '请输入正确的手机号'
+                }],
+              })
+              (<Input placeholder="请输入"/>)
+              }
+            </FormItem>
+          </Col>
+          <Col md={8} sm={24}>
+            <FormItem label="微信/QQ:">
+              {getFieldDecorator('wechat', {//【微信/QQ】支持中文、英文、数字，允许输入特殊字符，小写英文自动转换为大写，最多100个字符
+                initialValue: "",
+                rules: [{max: 32, message: '长度不能超过32'}],
+              })
+              (<Input placeholder="请输入"/>)
+              }
+            </FormItem>
+          </Col>
+        </Row>
+        <Row>
+          <Col span={12}>
+            <Button type="primary" onClick={this.showModal}>新增客户</Button>
+          </Col>
+          <Col span={12} style={{textAlign: 'right'}}>
+            <FormItem>
+              <Button type="primary" htmlType="submit">查询</Button>
+              <Button style={{marginLeft: 8}} onClick={this.handleFormReset}>重置</Button>
+            </FormItem>
+          </Col>
+        </Row>
+      </Form>
+    );
+  }
+
+  renderModalForm() {
+    const {getFieldDecorator} = this.props.form;
+
+    const formItemLayout = {
+      labelCol: {span: 4},
+      wrapperCol: {span: 20},
+    };
+    const formTailLayout = {
+      labelCol: {span: 4},
+      wrapperCol: {span: 20, offset: 4},
+    };
+
+    return (
+      <Form onSubmit={this.handleModalSave}>
+        <FormItem {...formItemLayout} label="客户名称:">
+          {getFieldDecorator('name1', { //【客户名称】支持中文、英文、数字，最多50个字符；
             initialValue: "",
             rules: [{max: 50, message: '长度不能超过50'}],
           })
-          (<Input placeholder="请输入" style={inputStyle}/>)
+          (<Input placeholder="请输入"/>)
           }
         </FormItem>
-        <FormItem label="联系人:" style={formItemStyle}>
-          {getFieldDecorator('connect', {//【联系人】支持中文、英文，允许输入特殊字符，小写英文自动转换为大写，最多20个字符；
+        <FormItem {...formItemLayout} label="联系人:">
+          {getFieldDecorator('connect1', {//【联系人】支持中文、英文，允许输入特殊字符，小写英文自动转换为大写，最多20个字符；
             initialValue: "",
             rules: [{max: 20, message: '长度不能超过20'}],
           })
-          (<Input placeholder="请输入" style={inputStyle}/>)
+          (<Input placeholder="请输入"/>)
           }
         </FormItem>
-        <FormItem label="手机号:" style={formItemStyle}>
-          {getFieldDecorator('mobile', {//【电话号码】支持数字，允许输入特殊字符，最多50个字符；
+        <FormItem {...formItemLayout} label="手机号:">
+          {getFieldDecorator('mobile1', {//【电话号码】支持数字，允许输入特殊字符，最多50个字符；
             initialValue: "",
             rules: [{
               pattern: /^\d{0,20}$/,
               message: '请输入正确的手机号'
             }],
           })
-          (<Input placeholder="请输入" style={inputStyle}/>)
+          (<Input placeholder="请输入"/>)
           }
         </FormItem>
-        <FormItem label="微信/QQ:" style={formItemStyle}>
-          {getFieldDecorator('wechat', {//【微信/QQ】支持中文、英文、数字，允许输入特殊字符，小写英文自动转换为大写，最多100个字符
+        <FormItem {...formItemLayout} label="微信/QQ:">
+          {getFieldDecorator('wechat1', {//【微信/QQ】支持中文、英文、数字，允许输入特殊字符，小写英文自动转换为大写，最多100个字符
             initialValue: "",
             rules: [{max: 32, message: '长度不能超过32'}],
           })
-          (<Input placeholder="请输入" style={inputStyle}/>)
+          (<Input placeholder="请输入"/>)
           }
         </FormItem>
-        <FormItem style={formItemStyle}>
-          <Button type="primary" htmlType="submit">查询</Button>
-          <Button style={{marginLeft: 8}} onClick={this.handleFormReset}>重置</Button>
+        <FormItem {...formTailLayout}>
+          <Button
+            type="primary"
+            htmlType="submit"
+          >
+            确认
+          </Button>
+          <Button
+            style={{marginLeft: 8}}
+            onClick={() => {
+              this.setState({visible: false});
+            }}
+          >
+            取消
+          </Button>
         </FormItem>
       </Form>
     );
+  }
+
+  showModal = () => {
+    this.setState({
+      visible: true,
+    });
+  }
+  handleOk = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
+  }
+  handleCancel = (e) => {
+    console.log(e);
+    this.setState({
+      visible: false,
+    });
   }
 
   render() {
@@ -183,6 +306,15 @@ export default class TableList extends PureComponent {
             />
           </div>
         </Card>
+        <Modal
+          title="新增客户"
+          visible={this.state.visible}
+          onOk={this.handleOk}
+          onCancel={this.handleCancel}
+          // footer={null}
+        >
+          {this.renderModalForm()}
+        </Modal>
       </PageHeaderLayout>
     );
   }
