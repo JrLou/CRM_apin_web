9a72ec314af3d1f154bc3526c4fd85e0fda899f3
lisiqi6911660 <lisiqi6911660@163.com>
2018-01-24 21:29:57 +0800

    修正样式

--- src/routes/Supplier/supplierPolicy/Flightstockfrom.js
+++ src/routes/Supplier/supplierPolicy/Flightstockfrom.js
@@ -912,7 +912,7 @@ class AddForm extends Component {
             visible={visible}
             onCancel={this.handleCancels.bind(this)}
             footer={null}
-            width={'900px'}
+            width={this.state.listAir == 1 ? '400px' : '1200px'}
           >
             {this.state.listAir == 1 && <TextArea placeholder="请填写" onChange={this.valHeadquarters.bind(this, 8)}/>}
             {this.state.listAir == 2 &&
